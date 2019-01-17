bc5421b0df95856c1a16d610e99d140ccbc0e6f2
r-zachary-murray <rpm233@cornell.edu>
2019-01-16 20:49:20 -0500

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,16 @@
+ALCDEF_read
+==========
+
+Provides an way of easily accessing asteroid light curves from the ALCDEF, asteroid lightcurve photometry database. 
+
+ 
+Installation
+-----------
+
+Download alcdef_read.py and place it in the same directory as your python script. Then import it as show in Example 1, the file used in the example is also available for download. 
+
+Usage
+------------
+
+When reading a file the readfile function returns a list of python dictionaries, one for each light curve 'block' in the file.  The 'header' dictionary contains information that can be found in that blocks header, and the data dictionary contains information labeled as data.  Keys accepted by the data dictionaries are ["TIME"], ["MAG"] and ["ERR"].  An exhaustive list of the keys accepted by the header dictionary can be found here: https://minorplanetcenter.net//iau/info/ALCDEF_V109.pdf
+
---
+++ alcdef_read.py
@@ -0,0 +1,50 @@
+import numpy as np
+import os
+from os import listdir
+from os.path import isfile, join
+
+def readfile(myfile):
+
+	with open(myfile) as f:
+	    lines = f.readlines()
+	    
+	lines = [line.rstrip('\r\n') for line in lines]
+
+	startlines =[]
+	endlines = []
+
+	for i in range(len(lines)):
+	    if lines[i] == 'STARTMETADATA':
+		startlines.append(i)
+	    if lines[i] == 'ENDMETADATA':
+		endlines.append(i)
+		
+	startlines.append(len(lines))
+
+	allheaders = []
+	alldata = []
+
+	for j in range(len(startlines)-1):
+
+	    header = {}
+	    data = {}
+
+	    for i in range(startlines[j]+1, endlines[j]):
+		dat = lines[i].split('=')
+		header[dat[0]]=dat[1]
+
+	    tmpray = []
+	    for i in range(endlines[j]+1, startlines[j+1]):
+		dat = lines[i].split('=')[1]
+		rows = dat.split('|')
+		tmpray.append(rows)
+
+	    tmpray = np.asarray(tmpray).T
+	    data["TIME"]  = tmpray[0].astype(float)
+	    data["MAG"]  = tmpray[1].astype(float)
+	    data["ERR"] = tmpray[2].astype(float)
+	    
+	    allheaders.append(header)
+	    alldata.append(data)
+
+	return allheaders, alldata
