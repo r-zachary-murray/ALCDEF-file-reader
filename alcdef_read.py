6e88c3b6ce0b7ac055065ae5535663238da67a19
Zach  Murray <rpm233@cornell.edu>
2020-11-17 19:20:23 -0500

    Add files via upload

--- alcdef_read.py
+++ alcdef_read.py
@@ -6,7 +6,7 @@ from os.path import isfile, join
 def readfile(myfile):
 
 	with open(myfile) as f:
-	    lines = f.readlines()
+		lines = f.readlines()
 	    
 	lines = [line.rstrip('\r\n') for line in lines]
 
@@ -14,10 +14,10 @@ def readfile(myfile):
 	endlines = []
 
 	for i in range(len(lines)):
-	    if lines[i] == 'STARTMETADATA':
-		startlines.append(i)
-	    if lines[i] == 'ENDMETADATA':
-		endlines.append(i)
+		if lines[i] == 'STARTMETADATA':
+			startlines.append(i)
+		if lines[i] == 'ENDMETADATA':
+			endlines.append(i)
 		
 	startlines.append(len(lines))
 
@@ -26,25 +26,25 @@ def readfile(myfile):
 
 	for j in range(len(startlines)-1):
 
-	    header = {}
-	    data = {}
+		header = {}
+		data = {}
 
-	    for i in range(startlines[j]+1, endlines[j]):
-		dat = lines[i].split('=')
-		header[dat[0]]=dat[1]
+		for i in range(startlines[j]+1, endlines[j]):
+			dat = lines[i].split('=')
+			header[dat[0]]=dat[1]
 
-	    tmpray = []
-	    for i in range(endlines[j]+1, startlines[j+1]):
-		dat = lines[i].split('=')[1]
-		rows = dat.split('|')
-		tmpray.append(rows)
+		tmpray = []
+		for i in range(endlines[j]+1, startlines[j+1]):
+			dat = lines[i].split('=')[1]
+			rows = dat.split('|')
+			tmpray.append(rows)
 
-	    tmpray = np.asarray(tmpray).T
-	    data["TIME"]  = tmpray[0].astype(float)
-	    data["MAG"]  = tmpray[1].astype(float)
-	    data["ERR"] = tmpray[2].astype(float)
-	    
-	    allheaders.append(header)
-	    alldata.append(data)
+		tmpray = np.asarray(tmpray).T
+		data["TIME"]  = tmpray[0].astype(float)
+		data["MAG"]  = tmpray[1].astype(float)
+		data["ERR"] = tmpray[2].astype(float)
+	
+		allheaders.append(header)
+		alldata.append(data)
 
 	return allheaders, alldata
